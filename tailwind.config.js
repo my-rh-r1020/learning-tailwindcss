defd9b989122a21afb95252180da998156ab39eb
my-rh-r1020 <rherikson.my@gmail.com>
2022-07-08 15:08:19 +0700

    Tailwind CSS - Utilities Part 3 Finish

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./utilities/*.{html}"],
+  content: ["./index.html", "./utilities/*.html"],
   darkMode: "class",
   theme: {
     extend: {
