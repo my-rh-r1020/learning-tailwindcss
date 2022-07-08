ac563018c6fa74d7d09e03ea2ebae1642847be49
my-rh-r1020 <rherikson.my@gmail.com>
2022-07-08 17:05:07 +0700

    Tailwind CSS - Utilities Part 4 Finish

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,13 @@ module.exports = {
       colors: {
         neonGreen: "#37E2D5",
       },
+      animation: {
+        "spin-slow": "spin 3s linear infinite",
+        goyang: "wiggle 1s ease-in-out infinite",
+      },
+      keyframes: {
+        wiggle: { "0%, 100%": { transform: "rotate(-3deg)" }, "50%": { transform: "rotate(3deg)" } },
+      },
     },
   },
   plugins: [],
